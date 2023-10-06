c86847d76cfbdfefbca47596d0733eabd062cc37
Sammy <sammy.liaop@gmail.com>
2023-10-05 20:29:33 -0400

    hello

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div>
+      <h1>Hello!</h1>
     </div>
-  );
+  )
 }
 
 export default App;
